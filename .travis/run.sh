879e150930a45fbf97c5011dc8b659888bf929b9
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-07-31 19:07:52 -0400

    Update run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -2,7 +2,7 @@
 
 if [ $2 = "2" ]; then
     echo "run python-nxsrecselector"
-    docker exec -it ndts python test/runtest.py $1 || true
+    docker exec -it ndts python test/runtest.py $1
 else
     echo "run python3-nxsrecselector"
     docker exec -it ndts python3 test/runtest.py $1
