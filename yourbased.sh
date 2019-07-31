767f6f7b1b0e8736ce1b84fa09a58d395ff584b1
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-07-31 19:55:32 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -5,6 +5,7 @@ chmod +x .travis/run.sh
 chmod +x .travis/install.sh
 docker rm --force ndts || true
 docker build -t ndts .travis/debian9_py2
+pwd
 docker run  --name  ndts -d -it -v `pwd`:/home/tango  ndts
 .travis/install.sh debain9 2
 .travis/run.sh basic 2
