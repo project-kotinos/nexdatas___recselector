8bd33fa0fe47fdcbde7f43e2258ea467f2446dfe
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2019-07-31 20:48:20 -0400

    Update yourbased.sh

--- yourbased.sh
+++ yourbased.sh
@@ -8,6 +8,7 @@ docker build -t ndts .travis/${OS}_py${PY}
 pwd
 ls
 docker run  --name  ndts -d -it -v `pwd`:/home/tango  ndts
+docker exec --user root ndts /bin/sh -c 'pwd; ls'
 .travis/install.sh ${OS} ${PY}
 .travis/run.sh ${TEST} ${PY}
 docker stop ndts
